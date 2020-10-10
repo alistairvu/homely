f2cdd90a367ad608b2926f31f069b1bfaafb85fa
Tran Hung <Hungtran-pro@users.noreply.github.com>
2020-10-10 15:51:12 +0700

    userlist

--- js/user.js
+++ js/user.js
@@ -1,6 +1,34 @@
-const user = {
-    idUser1 : 1,
-    idUser2 : 2,
-    idUser3 : 3,
-    idUser4 : 4,
+const user = [
+  {
+    name: 'User1',
+    id: 1,
+  },
+  {
+    name: 'User2',
+    id: 2,
+  },
+  {
+    name: 'User3',
+    id: 3,
+  },
+  {
+    name: 'User4',
+    id: 4,
+  },
+];
+
+function viewUsers() {
+    let userBody = document.getElementById('list-users');
+    for(let i = 0; i < user.length; i++){
+        const user = userBody[i];
+        let displayUser = {
+            `<tr>
+                <td>${user.name}</td>
+                <td>${user.id}</span></td>
+            </tr>`
+        }
+        userBody.innerHTML += displayUser;
+    }
 }
+
+viewUsers();
