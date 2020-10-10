b653c7c3cab066c931698ab4a8af02299632e40a
Tran Hung <Hungtran-pro@users.noreply.github.com>
2020-10-10 21:18:45 +0700

    bla

--- js/user.js
+++ js/user.js
@@ -1,18 +1,22 @@
 const user = [
   {
     name: "User1",
+    password: "blabla",
     id: 1,
   },
   {
     name: "User2",
+    password: "blabla",
     id: 2,
   },
   {
     name: "User3",
+    password: "blabla",
     id: 3,
   },
   {
     name: "User4",
+    password: "blabla",
     id: 4,
   },
 ];
