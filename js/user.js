961d676bf0f0709ebd71ddb16f553c6782071bb7
Alistair Vu <alistairvu@gmail.com>
2020-10-10 19:08:46 +0700

    Fix user display

--- js/user.js
+++ js/user.js
@@ -1,34 +1,33 @@
 const user = [
   {
-    name: 'User1',
+    name: "User1",
     id: 1,
   },
   {
-    name: 'User2',
+    name: "User2",
     id: 2,
   },
   {
-    name: 'User3',
+    name: "User3",
     id: 3,
   },
   {
-    name: 'User4',
+    name: "User4",
     id: 4,
   },
 ];
 
 function viewUsers() {
-    let userBody = document.getElementById('list-users');
-    for(let i = 0; i < user.length; i++){
-        const user = userBody[i];
-        let displayUser = {
-            `<tr>
-                <td>${user.name}</td>
-                <td>${user.id}</span></td>
-            </tr>`
-        }
-        userBody.innerHTML += displayUser;
-    }
+  let userBody = document.getElementById("list-users");
+  for (let i = 0; i < user.length; i++) {
+    const userNum = user[i];
+    console.log(userNum);
+    let displayUser = `<tr>
+                <td>${userNum.name}</td>
+                <td>${userNum.id}</span></td>
+            </tr>`;
+    userBody.innerHTML += displayUser;
+  }
 }
 
 viewUsers();
