f443c3e3f658712216617a37289a0062ef1f4fc6
Tran Hung <Hungtran-pro@users.noreply.github.com>
2020-10-10 08:34:35 +0700

    đang xử lí :v:

---
+++ js/housedata.js
@@ -0,0 +1,99 @@
+const HOUSE = [
+  {
+    houseImg:
+      "https://file4.batdongsan.com.vn/resize/200x200/2020/05/07/vVMfcTGI/20200507155707-2542_wm.jpg",
+    housePrice: 4.2,
+    houseBedroom: 3,
+    houseBathroom: 2,
+    houseFloor: 4,
+    housePhone: "+84 1234567",
+    houseSquare: 40,
+    houseAddress: "So 1 Khu A phố B ",
+    houseAddDis: "Thanh Xuân",
+    houseAddCity: "Hà Nội",
+    houseTittle: `Cực hiếm, 35 m2 lô góc, 3 mặt thoáng, oto vào nhà, 5 tầng xây mới. Ngõ 175 Lạc Long Quân, Nghĩa Đô, Cầu Giấy`,
+    houseDescript: `Nhà vị trí cực đẹp, cực hiếm.
+            Xây 5 tầng khung bê tông chắc chắn, bao đẹp, NT toàn gỗ Lim nguyên khối`,
+  },
+  {
+    houseImg:
+      "https://file4.batdongsan.com.vn/resize/745x510/2020/08/21/IDHysJ7g/20200821081441-b4e0_wm.jpg",
+    housePrice: 5.5,
+    houseBedroom: 5,
+    houseBathroom: 3,
+    houseFloor: 3,
+    housePhone: "+84 1534567",
+    houseSquare: 40,
+    houseAddress: "So 2 Khu B phố C ",
+    houseAddDis: "Hai Bà Trưng",
+    houseAddCity: "Hà Nội",
+    houseTittle: `Nhà 3 tầng 1 tỷ 50tr sổ đỏ bao tên đối diện AEON Mall Hà Đông`,
+    houseDescript: `Nhà vị trí cực đẹp, cực hiếm.
+            Xây 5 tầng khung bê tông chắc chắn, bao đẹp, NT toàn gỗ Lim nguyên khối`,
+  },
+  {
+    houseImg:
+      "https://file4.batdongsan.com.vn/resize/745x510/2020/09/14/20200914161906-2702_wm.jpg",
+    housePrice: 2.1,
+    houseBedroom: 2,
+    houseBathroom: 1,
+    houseFloor: 2,
+    housePhone: "+84 1235567",
+    houseSquare: 35,
+    houseAddress: "So 3 Khu C phố D ",
+    houseAddDis: "Quận 1",
+    houseAddCity: "Hồ Chí Minh",
+    houseTittle: `CHÍNH CHỦ BÁN NHÀ XÂY MỚI 32M2 X 5 TẦNG TẠI TƯ ĐÌNH LONG BIÊN`,
+    houseDescript: `Tôi cần bán căn nhà 5 tầng xây mới diện tích 32m2 căn góc 2 mặt thoáng (DTSD 160m2) tại ngõ 129 đường Bát Khối, khu Tư Đình, Long Biên giá 2,5 tỷ đồng.
+            Tầng 1: Phòng khách và bếp ( bếp đã lắp đặt tủ bếp, hút mùi và bếp từ đôi, chậu rửa bát hai hố và vòi rửa bát nóng lạnh)
+            Tầng 2 + tầng 3 + tầng 4: mỗi tầng có 1 phòng ngủ lớn 18m2 lát sàn gỗ và điều hòa các tầng cùng 1 phòng vệ sinh sang trọng với bộ thiết bị cao cấp, nóng lạnh các tầng và cabin tắm.
+            Tầng 5: Sân phơi rộng lớn được che bởi tôn mát 2 lớp và sắt bảo vệ xung quanh.
+            Hướng nhà: Đông Bắc
+            Mặt tiền rộng: 4,2m, ngõ rộng 2,3m
+            Căn nhà nằm trong khu dân trí cao và gần chợ, gần trường học các cấp. Căn nhà có thiết kế đầy đủ và chi tiết, sổ đỏ chính chủ.
+            Hình ảnh kèm theo là hình ảnh thực tế căn nhà.
+            Hỗ trợ vay NH lên tối đa 75% với lãi suất 7%/ năm.`,
+  },
+  {
+    houseImg:
+      "https://file4.batdongsan.com.vn/resize/745x510/2020/10/09/20201009135808-ea5f_wm.jpg",
+    housePrice: 33.5,
+    houseBedroom: 4,
+    houseBathroom: 4,
+    houseFloor: 2,
+    housePhone: "+84 1235567",
+    houseSquare: 31,
+    houseAddress: "Phố Lý Thường Kiệt, Phường Trần Hưng Đạo, Hoàn Kiếm, Hà Nội",
+    houseAddDis: "Hoàn Kiếm",
+    houseAddCity: "Hà Nội",
+    houseTittle: `BÁN NHÀ MẶT PHỐ LÝ THƯỜNG KIỆT, MẶT TIỀN 4M, ĐANG CHO THUÊ 100TRIỆU/THÁNG GIÁ 33,5 TỶ`,
+    houseDescript: `Bán nhà mặt phố tại số 39 Lý Thường Kiệt, quận Hoàn Kiếm, thành phố Hà Nội.
+
+            Vị trí đắc địa nằm ngay trung tâm thành phố, dân cư đông đúc, giao thông thuận tiện, đường 2 chiều oto, mặt tiền cực thoáng, vỉa hè rộng, khu dân trí cao. Tiện mở văn phòng, kinh doanh..
+            
+            Diện tích sổ đỏ: 31,2m2 x 4 tầng.
+            Mặt tiền: 4m.
+            Hiẹn đang cho thuê 100 triệu/tháng`,
+  },
+  {
+    houseImg:
+      "https://file4.batdongsan.com.vn/resize/745x510/2020/10/09/20201009135808-ea5f_wm.jpg",
+    housePrice: 33.5,
+    houseBedroom: 4,
+    houseBathroom: 4,
+    houseFloor: 2,
+    housePhone: "+84 1235567",
+    houseSquare: 31,
+    houseAddress: "Phố Lý Thường Kiệt, Phường Trần Hưng Đạo, Hoàn Kiếm, Hà Nội",
+    houseAddDis: "Hoàn Kiếm",
+    houseAddCity: "Hà Nội",
+    houseTittle: `BÁN NHÀ MẶT PHỐ LÝ THƯỜNG KIỆT, MẶT TIỀN 4M, ĐANG CHO THUÊ 100TRIỆU/THÁNG GIÁ 33,5 TỶ`,
+    houseDescript: `Bán nhà mặt phố tại số 39 Lý Thường Kiệt, quận Hoàn Kiếm, thành phố Hà Nội.
+    
+            Vị trí đắc địa nằm ngay trung tâm thành phố, dân cư đông đúc, giao thông thuận tiện, đường 2 chiều oto, mặt tiền cực thoáng, vỉa hè rộng, khu dân trí cao. Tiện mở văn phòng, kinh doanh..
+                
+            Diện tích sổ đỏ: 31,2m2 x 4 tầng.
+            Mặt tiền: 4m.
+            Hiẹn đang cho thuê 100 triệu/tháng`,
+  },
+];
