fa1b9fbd85f4f6b75e3ad30dee6c0820b2e37caf
Vu Ho <vuho23@gmail.com>
2020-10-12 10:32:31 +0700

    add listing-upload

--- js/listing-upload.js
+++ js/listing-upload.js
@@ -8,11 +8,32 @@ cityPick.onchange = function () {
     case "Hanoi":
       districtOptions.insertAdjacentHTML(
         "afterend",
+        
         `
       <option class="districtHN">Cầu Giấy</option>
       <option class="districtHN">Thanh Xuân</option>
       <option class="districtHN">Hoàn Kiếm</option>`
       );
       break;
+    case "Ho Chi Minh City":
+      districtOptions.insertAdjacentHTML(
+        "afterend",
+        
+        `
+      <option class="districtHCM">Quận 1</option>
+      <option class="districtHCM">Quận 2</option>`
+      );
+      break;
+    case "Da Nang":
+      districtOptions.insertAdjacentHTML(
+        "afterend",
+        
+        `
+      <option class="districtDN">Sơn Trà</option>
+      <option class="districtDN">Hải Châu</option>`
+      
+      );
+      break;
   }
 };
+  
