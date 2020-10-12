d9119580c93029d00dea43aa94c63e74bbe70aeb
Tran Hung <Hungtran-pro@users.noreply.github.com>
2020-10-12 10:41:42 +0700

    userpage-tmp

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 function reportUser() {
-    if(confirm("Choose a button")){
-      alert('Report sent!');
-    }
-    else{
-      alert('Cancel done!')
-    }
+  if (confirm("Choose a button")) {
+    alert("Report sent!");
+  } else {
+    alert("Cancel done!");
   }
+}
+
+function viewUsers() {
+  let userBody = document.getElementById("list-users");
+  for (let i = 0; i < user.length; i++) {
+    const userNum = user[i];
+    let displayUser = `<tr>
+    <th scope="row">${i + 1}</th>
+    <td>${userNum.name}</td>
+    <td>${userNum.id}</td>
+    <td>
+      <button type="button" class="btn btn-danger " onclick = "reportUser()">Report!</button>
+    </td>
+      </tr>`;
+    userBody.innerHTML += displayUser;
+  }
+}
+
+viewUsers();
