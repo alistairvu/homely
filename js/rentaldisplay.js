29760168f958d799519579ac4ab9c3290a4491e1
Alistair Vu <alistairvu@gmail.com>
2020-10-12 14:39:41 +0700

    Set three listings in one row

--- js/rentaldisplay.js
+++ js/rentaldisplay.js
@@ -4,31 +4,32 @@ const limit = RENT.length;
 const houseResults = document.getElementById("house-results");
 
 for (let i = 0; i < limit; i += 3) {
-  let listing = RENT[i];
-  console.log(i);
-  if (i % 3 == 0) {
-    houseResults.innerHTML += `<div class="row">`;
-    console.log("New row added.");
+  let listingHTML = ``;
+  for (let j = i; j < i + 3; j++) {
+    if (j < limit) {
+      let listing = RENT[i];
+      listingHTML += `
+      <div class="card col-sm-4">
+        <div class="card-body">
+          <img src="${listing.rentImg} class="card-img-top"/>
+          <h5 class="listing-title"> <a href="listing-sample.html"> ${listing.rentDescript}</a> </h5>
+          <p class="brief-info">
+            Price: $${listing.rentPrice}/mo | Bedrooms: ${listing.rentBedroom} <br />
+            Bathrooms: ${listing.rentBathroom} | Area: ${listing.rentSquare} m<sup>2</sup>
+          </p>
+          <p style="font-family: 'Playfair Display', serif">
+            ${listing.rentAddDis}, ${listing.rentAddCity}
+          </p>
+          <p class="listing-description">
+            ${listing.rentDescript}
+          </p>
+          </div>
+        </div>
+      `;
+    }
   }
   houseResults.innerHTML += `
-  <div class="card col-sm-4">
-    <div class="card-body">
-      <img src="${listing.rentImg} class="card-img-top"/>
-      <h5> <a href="listing-sample.html" class="listing-title"> ${listing.rentDescript}</a> </h5>
-      <p class="brief-info">
-        Price: $${listing.rentPrice}/mo | Bedrooms: ${listing.rentBedroom} <br />
-        Bathrooms: ${listing.rentBathroom} | Area: ${listing.rentSquare} m<sup>2</sup>
-      </p>
-      <p style="font-family: 'Playfair Display', serif">
-        ${listing.rentAddDis}, ${listing.rentAddCity}
-      </p>
-      <p class="listing-description">
-        ${listing.rentAddDis}
-      </p>
-      </div>
-    </div>
-  `;
-  if (i % 3 == 2 || i == limit - 1) {
-    houseResults.innerHTML += `</div>`;
-  }
+  <div class="row">
+  ${listingHTML}
+  </div>`;
 }
