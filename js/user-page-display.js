cfcb05488812a17196e9a59b34b2a3b93a57a525
Tran Hung <Hungtran-pro@users.noreply.github.com>
2020-10-13 22:10:48 +0700

    95% user+admin page

--- js/user-page-display.js
+++ js/user-page-display.js
@@ -1,35 +1,35 @@
-import { HOUSE } from './housedata.js';
-import { rent } from './rentdata.js';
+import { HOUSE } from "./housedata.js";
+import { rent } from "./rentdata.js";
 
 let inforHouse = document.getElementById("list-houses-user");
 const ID = 2;
 for (let i = 0; i < HOUSE.length; i++) {
-    const houseNum = HOUSE[i];
-    let displayInfor = `
+  const houseNum = HOUSE[i];
+  let displayInfor = `
         <div class="card col-sm-3">
-        <div class="card-body">
+        <div class="card-body" >
           <img
             src="${houseNum.houseImg}"
             alt="house"
             class="card-img-top"
           />
-          <h5>
-            <a href="#">Giao bán nhà mới</a>
+          <h5 class="listing-title">
+            <a href="#">${houseNum.houseTitle}</a>
           </h5>
-          <p class="brief-info">
-            Price: $1000 | Bedroom: 2 <br />
-            Bathrooms: 2 | Area: 100m <sup>2</sup>
+          <p class="brief-info" >
+          Price: ${houseNum.housePrice}m/month | Bedroom: ${houseNum.houseBedroom} <br />
+          Bathrooms: ${houseNum.houseBathroom} | Area: ${houseNum.houseSquare}m <sup>2</sup>
           </p>
-          <button disabled="disabled" >Deleted</button>
         </div>
+        <button class="align-self-end" style="margin: 9px auto;">Deleted</button>
       </div>`;
-    inforHouse.innerHTML += displayInfor;
+  inforHouse.innerHTML += displayInfor;
 }
 
 inforHouse = document.getElementById("list-houses-user");
 for (let i = 0; i < rent.length; i++) {
-    const rentNum = rent[i];
-    let displayInfor = `
+  const rentNum = rent[i];
+  let displayInfor = `
         <div class="card col-sm-3">
         <div class="card-body">
           <img
@@ -37,15 +37,15 @@ for (let i = 0; i < rent.length; i++) {
             alt="house"
             class="card-img-top"
           />
-          <h5>
-            <a href="#">Giao bán nhà mới</a>
+          <h5 class="listing-title">
+            <a href="#">${rentNum.rentTitle}</a>
           </h5>
           <p class="brief-info">
-            Price: $1000 | Bedroom: 2 <br />
-            Bathrooms: 2 | Area: 100m <sup>2</sup>
+            Price: ${rentNum.rentPrice}m/month | Bedroom: ${rentNum.rentBedroom} <br />
+            Bathrooms: ${rentNum.rentBathroom} | Area: ${rentNum.rentSquare}m <sup>2</sup>
           </p>
-          <button disabled="disabled" >Deleted</button>
-        </div>
+          </div>
+          <button disabled="disabled" class="align-self-end" style="margin: 0px auto 9px;">Deleted</button>
       </div>`;
-    inforHouse.innerHTML += displayInfor;
+  inforHouse.innerHTML += displayInfor;
 }
