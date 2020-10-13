648ba639bf28d24db07098eb6b76adc8061969b3
Tran Hung <Hungtran-pro@users.noreply.github.com>
2020-10-13 20:13:06 +0700

    fixed-admin-page

--- js/userdata.js
+++ js/userdata.js
@@ -21,17 +21,10 @@ const user = [
   },
 ];
 
-function reportUser() {
-  if (confirm("Choose a button")) {
-    alert("Report sent!");
-  } else {
-    alert("Cancel done!");
-  }
-}
-  let userBody = document.getElementById("list-users");
-  for (let i = 0; i < user.length; i++) {
-    const userNum = user[i];
-    let displayUser = `<tr>
+let userBody = document.getElementById("list-users");
+for (let i = 0; i < user.length; i++) {
+  const userNum = user[i];
+  let displayUser = `<tr>
     <th scope="row">${i + 1}</th>
     <td>${userNum.name}</td>
     <td>${userNum.id}</td>
@@ -39,5 +32,13 @@ function reportUser() {
       <button type="button" class="btn btn-danger " onclick = "reportUser()">Report!</button>
     </td>
       </tr>`;
-    userBody.innerHTML += displayUser;
+  userBody.innerHTML += displayUser;
+}
+
+function reportUser() {
+  if (confirm("Choose a button")) {
+    alert("Report sent!");
+  } else {
+    alert("Cancel done!");
   }
+}
