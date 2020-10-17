7945af357629d55a7490da973247fb3d9567a08b
Tran Hung <Hungtran-pro@users.noreply.github.com>
2020-10-17 20:35:34 +0700

    fix accountdata

--- js/accounts.js
+++ js/accounts.js
@@ -2,13 +2,26 @@ export const accounts = [
   {
     name: "admin",
     pass: "admin123",
-    id: "1",
     role: "admin",
   },
   {
-    name: "User",
-    pass: "blabla",
-    id: "2",
-    role: "user",
+    name: "User1",
+    password: "blabla",
+    id: 1,
+  },
+  {
+    name: "User2",
+    password: "blabla",
+    id: 2,
+  },
+  {
+    name: "User3",
+    password: "blabla",
+    id: 3,
+  },
+  {
+    name: "User4",
+    password: "blabla",
+    id: 4,
   },
 ];
