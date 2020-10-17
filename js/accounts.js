29966ca12112cb3bde2708bb1a2fbc543e94f074
quyetdangtran <quyetdt99.hust@gmail.com>
2020-10-17 19:54:57 +0700

    change commit

---
+++ js/accounts.js
@@ -0,0 +1,14 @@
+export const accounts = [
+  {
+    name: "admin",
+    pass: "admin123",
+    id: "1",
+    role: "admin",
+  },
+  {
+    name: "User",
+    pass: "blabla",
+    id: "2",
+    role: "user",
+  },
+];
---
+++ js/register.js
@@ -0,0 +1,7 @@
+let register = document.getElementById('register-box');
+let newUserName = document.getElementById('name');
+let newPassword = document.getElementById('pass')
+let confirmPassword= document.getElementById('confirmPassword')
+let email = document.getElementById('mail');
+let fullName = document.getElementById('full-name');
+let createbtn = document.getElementById('create-btn');
