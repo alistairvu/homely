207b5043bfa38d136f9231fef211835c51e3def3
quyetdangtran <quyetdt99.hust@gmail.com>
2020-10-17 21:39:08 +0700

    add discrible

--- js/accounts.js
+++ js/accounts.js
@@ -3,12 +3,15 @@ export const accounts = [
     name: "admin",
     pass: "admin123",
     id: "1",
+    email: 'admin@gmail.com',
     role: "admin",
+
   },
   {
     name: "User",
     pass: "blabla",
     id: "2",
+    email:'user@gmail.com',
     role: "user",
   },
 ];
--- js/register.js
+++ js/register.js
@@ -1,7 +1,17 @@
+import { accounts } from "./accounts.js";
+
 let register = document.getElementById('register-box');
 let newUserName = document.getElementById('name');
 let newPassword = document.getElementById('pass')
 let confirmPassword= document.getElementById('confirmPassword')
 let email = document.getElementById('mail');
 let fullName = document.getElementById('full-name');
-let createbtn = document.getElementById('create-btn');
+let createBtn = document.getElementById('create-btn');
+
+function formRegister(){
+    if(newUserName.value !== accounts[i].name 
+        && newPassword.value == confirmPassword.Value
+        && email.value !== ){
+
+    } 
+}
