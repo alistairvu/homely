baeab2ceea56120c8f85acad202f98890c1eb1fc
Alistair Vu <alistairvu@gmail.com>
2020-10-17 20:14:23 +0700

    Add login.js script

--- js/login.js
+++ js/login.js
@@ -1,20 +1,29 @@
-import { accounts } from "./accounts.js"
+import { accounts } from "./accounts.js";
 
-let login = document.getElementById('login-box');
-let username = document.getElementById('username');
-let password = document.getElementById('password');
-let submitbtn = document.getElementById('submit-btn');
+let login = document.getElementById("login-box");
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let submitbtn = document.getElementById("submit-btn");
 
-function formLogIn (){
-for(let i = 0; i<accounts.length; i++ ){
-    if(username == accounts[i].name && password == accounts[i].password){
-        if(accounts[i].role =='admin'){
-            alert('hi admin')
-        }else if(accounts[i].role== 'user'){
-            alert(`Wellcome: ${accounts[i]}.name`)
-        }
+console.log(accounts);
+
+function formLogIn() {
+  console.log("Running...");
+  console.log(username.value);
+  let matches = accounts.filter((x) => x.name == username.value);
+  const match = matches[0];
+  if (password.value == match.pass) {
+    switch (match.role) {
+      case "admin":
+        alert("Hello, admin!");
+        break;
+      case "user":
+        alert("Welcome, user");
+        break;
     }
+  } else {
+    alert("Wrong password!");
+  }
 }
-}
-  
-  
+
+submitbtn.addEventListener("click", formLogIn);
