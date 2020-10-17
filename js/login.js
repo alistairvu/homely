43d78dc18e416c8076d42d4b41e34bdfca056e16
Alistair Vu <alistairvu@gmail.com>
2020-10-17 20:14:49 +0700

    Delete redundant lines

--- js/login.js
+++ js/login.js
@@ -1,6 +1,5 @@
 import { accounts } from "./accounts.js";
 
-let login = document.getElementById("login-box");
 let username = document.getElementById("username");
 let password = document.getElementById("password");
 let submitbtn = document.getElementById("submit-btn");
@@ -8,8 +7,6 @@ let submitbtn = document.getElementById("submit-btn");
 console.log(accounts);
 
 function formLogIn() {
-  console.log("Running...");
-  console.log(username.value);
   let matches = accounts.filter((x) => x.name == username.value);
   const match = matches[0];
   if (password.value == match.pass) {
