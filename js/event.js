8fef1de3feb6d9c32659add5a8db1de9a2aac9fe
quyetdangtran <quyetdt99.hust@gmail.com>
2020-10-17 17:43:48 +0700

    Update event.js

---
+++ js/event.js
@@ -0,0 +1,23 @@
+const Accounts= [
+    {
+        
+        name: "admin",
+        password: "admin123",
+        id: '1',
+        role: 'admin'
+      },
+    {
+        
+      name: "User",
+      password: "blabla",
+      id: '2',
+      role: 'user'
+    },
+    
+  ];
+
+const loginForm = document.getElementById('login-form');
+const registerForm = document.getElementById('register-form');
+const 
+  
+  
