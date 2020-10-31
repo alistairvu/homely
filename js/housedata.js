c6e99e4aa394e778ee5c1e19641a476fdfc928c6
Alistair Vu <alistairvu@gmail.com>
2020-10-31 19:39:56 +0700

    Adjust files

--- js/housedata.js
+++ js/housedata.js
@@ -1,134 +1,5 @@
 export const house = [
   {
-    houseid: 1,
-    houseImg:
-      "https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    housePrice: 4.2,
-    houseBedroom: 3,
-    houseBathroom: 2,
-    houseFloor: 4,
-    housePhone: "+84 1234567",
-    houseSquare: 40,
-    houseAddress: "Số 28 ngõ 302 Nguyễn Trãi ",
-    houseAddDis: "Thanh Xuân",
-    houseAddCity: "Hà Nội",
-    houseTitle: `Cực hiếm, 40 m2 lô góc, 3 mặt thoáng, oto vào nhà, 4 tầng xây mới. Ngõ 302 Nguyễn Trãi, Thanh Xuân, Hà Nội`,
-    houseDescript: ` BÁN NHÀ ĐẸP CHÍNH CHỦ QUẬN THANH XUÂN. 
-         
-         Cần bán nhà chính chủ 4x10 `,
-  },
-  {
-    houseid: 1,
-    houseImg:
-      "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    housePrice: 5.5,
-    houseBedroom: 5,
-    houseBathroom: 3,
-    houseFloor: 3,
-    housePhone: "+84 1534567",
-    houseSquare: 40,
-    houseAddress: "So 2 Khu B phố C",
-    houseAddDis: "Thanh Xuân",
-    houseAddCity: "Hà Nội",
-    houseTitle: `Nhà 3 tầng 1 tỷ 50tr sổ đỏ bao tên đối diện AEON Mall Hà Đông`,
-    houseDescript: `Nhà vị trí cực đẹp, cực hiếm. <br>
-            Xây 5 tầng khung bê tông chắc chắn, bao đẹp, NT toàn gỗ Lim nguyên khối`,
-  },
-  {
-    houseid: 2,
-    houseImg:
-      "https://images.unsplash.com/photo-1503174971373-b1f69850bded?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1087&q=80",
-    housePrice: 2.1,
-    houseBedroom: 2,
-    houseBathroom: 1,
-    houseFloor: 2,
-    housePhone: "+84 1235567",
-    houseSquare: 35,
-    houseAddress: "So 3 Khu C phố D ",
-    houseAddDis: "Quận 1",
-    houseAddCity: "Hồ Chí Minh",
-    houseTitle: `CHÍNH CHỦ BÁN NHÀ XÂY MỚI 32M2 X 5 TẦNG TẠI TƯ ĐÌNH LONG BIÊN`,
-    houseDescript: `Tôi cần bán căn nhà 5 tầng xây mới diện tích 32m2 căn góc 2 mặt thoáng (DTSD 160m2) tại ngõ 129 đường Bát Khối, khu Tư Đình, Long Biên giá 2,5 tỷ đồng. <br>
-            Tầng 1: Phòng khách và bếp ( bếp đã lắp đặt tủ bếp, hút mùi và bếp từ đôi, chậu rửa bát hai hố và vòi rửa bát nóng lạnh) <br>
-            Tầng 2 + tầng 3 + tầng 4: mỗi tầng có 1 phòng ngủ lớn 18m2 lát sàn gỗ và điều hòa các tầng cùng 1 phòng vệ sinh sang trọng với bộ thiết bị cao cấp, nóng lạnh các tầng và cabin tắm. <br>
-            Tầng 5: Sân phơi rộng lớn được che bởi tôn mát 2 lớp và sắt bảo vệ xung quanh. <br>
-            Hướng nhà: Đông Bắc <br>
-            Mặt tiền rộng: 4,2m, ngõ rộng 2,3m <br>
-            Căn nhà nằm trong khu dân trí cao và gần chợ, gần trường học các cấp. Căn nhà có thiết kế đầy đủ và chi tiết, sổ đỏ chính chủ. <br>
-            Hình ảnh kèm theo là hình ảnh thực tế căn nhà. <br>
-            Hỗ trợ vay NH lên tối đa 75% với lãi suất 7%/ năm.`,
-  },
-  {
-    houseid: 2,
-    houseImg:
-      "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=973&q=80",
-    housePrice: 33.5,
-    houseBedroom: 4,
-    houseBathroom: 4,
-    houseFloor: 2,
-    housePhone: "+84 1235567",
-    houseSquare: 31,
-    houseAddress: "Phố Lý Thường Kiệt, Phường Trần Hưng Đạo",
-    houseAddDis: "Hoàn Kiếm",
-    houseAddCity: "Hà Nội",
-    houseTitle: `BÁN NHÀ MẶT PHỐ LÝ THƯỜNG KIỆT, MẶT TIỀN 4M, ĐANG CHO THUÊ 100TRIỆU/THÁNG GIÁ 33,5 TỶ`,
-    houseDescript: `Bán nhà mặt phố tại số 39 Lý Thường Kiệt, quận Hoàn Kiếm, thành phố Hà Nội. <br>
-
-            Vị trí đắc địa nằm ngay trung tâm thành phố, dân cư đông đúc, giao thông thuận tiện, đường 2 chiều oto, mặt tiền cực thoáng, vỉa hè rộng, khu dân trí cao. Tiện mở văn phòng, kinh doanh... <br>
-            
-            Diện tích sổ đỏ: 31,2m2 x 4 tầng. <br>
-            Mặt tiền: 4m. <br>
-            Hiẹn đang cho thuê 100 triệu/tháng`,
-  },
-  {
-    houseid: 3,
-    houseImg:
-      "https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80",
-    housePrice: 20,
-    houseBedroom: 7,
-    houseBathroom: 5,
-    houseFloor: 3,
-    housePhone: "+84 1235567",
-    houseSquare: 186.4,
-    houseAddress: "Phố Lý Thường Kiệt, Phường Trần Hưng Đạo",
-    houseAddDis: "Hoàn Kiếm",
-    houseAddCity: "Hà Nội",
-    houseTitle: `100% CHÍNH CHỦ BÁN NHANH SONG LẬP ÁP GÓC KHU TULIP 03 - 32 TÂY BẮC 20 TỶ`,
-    houseDescript: `Sở hữu rất nhiều tiện ích như: <br>
-    - Cluhouse rộng tới gần 10.000m2, hồ điều 12.4Ha cách 50m. <br>
-    - Quảng trường trung tâm hướng hồ điều hòa. <br>
-    - Khu thể thao, sân tập golf, sân tennis, công viên, vườn hoa. <br>
-    - Khu vui chơi trẻ em, đường dạo bộ quanh hồ điều hòa, vườn nướng BBQ. <br>
-    - Trường liên cấp I - II Vinschool. <br>
-    - Xung quanh đã có nhiều gia đình dọn đến ở, khu dân trí cao. <br>
-    - Khu vực yên tĩnh và rất an ninh Không gian sống trong lành.`,
-  },
-  {
-    houseid: 3,
-    houseImg:
-      "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80",
-    housePrice: 7.2,
-    houseBedroom: 4,
-    houseBathroom: 3,
-    houseFloor: 2,
-    housePhone: "+84 1235567",
-    houseSquare: 90,
-    houseAddress:
-      "Thống Nhất Complex, số 82, Đường Nguyễn Tuân, Phường Thanh Xuân Trung",
-    houseAddDis: "Thanh Xuân",
-    houseAddCity: "Hà Nội",
-    houseTitle: `Bán liền kề 88m2 thấp tầng tổ hợp nhà LK trung tâm thương mại Thống Nhất Complex số 82 Nguyễn Tuân`,
-    houseDescript: `Bán liền kề 88m2 nhà ở thấp tầng tổ hợp nhà liền kề trung tâm thương mại và nhà ở Thống Nhất Complex số 82 Nguyễn Tuân - Quận Thanh Xuân - Hà Nội. <br>
-    - Chủ đầu tư Công Ty Thống Nhất Bắc Việt. <br>
-    - Vị trí số 82 đường Nguyễn Tuân - Thanh Xuân. <br>
-    - Diện tích: 88m2 xây 5 tầng có thang máy. <br>
-    - Hướng Đông Nam. <br>
-    - Tổng diện tích xây dựng 450m2. <br>
-    - Mật độ xây dựng: 85%. <br>
-    - Đường phân lô 12.5m. <br>
-    Vị trí cách đường Nguyễn Tuân 20m.`,
-  },
-  {
     houseid: 3,
     houseImg:
       "https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80",
@@ -186,5 +57,136 @@ export const house = [
     Lakeview City với vị trí thuận lợi giao thông với các khu vực lân cận và sân bay Long Thành khi xây dựng xong,<br>
     Lakeview City là một trong những dự án có diện tích lớn ở cửa ngõ phía đông thành phố với tổng diện tích 30,1 ha mật độ xây dựng 60% còn lại 40% dành cho cây xanh và các công trình tiện ích dành cho khu đô thị. <br>
     Hiện tại view hồ chỉ còn một căn duy nhất chốt giá 15 tỷ khách hàng quan tâm gọi ngay`,
-  }
+  },
+  {
+    rentid: 4,
+    rentImg:
+      "https://images.unsplash.com/photo-1449844908441-8829872d2607?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    rentPrice: 3.1,
+    rentBedroom: 2,
+    rentBathroom: 2,
+    rentFloor: 3,
+    rentPhone: "+84 1235567",
+    rentSquare: 65,
+    rentAddress: "Hiyori Garden Tower, Đường Võ Văn Kiệt, Phường An Hải Đông",
+    rentAddDis: "Sơn Trà",
+    rentAddCity: "Đà Nẵng",
+    rentTitle: `Bán căn hộ Hiyori tiêu chuẩn Nhật Bản, khả năng cho thuê tốt`,
+    rentDescript: `Vị trí: 4 mặt tiền tuyến đường vàng Võ Văn Kiệt, kết nối sân bay biển thành phố Hội An.
+    Vị trí nằm giữa cung đường huyết mạch thẳng từ Nguyễn Văn Linh đến Võ Văn Kiệt ra biển Mỹ Khê, chỉ mất 5 phút đi bộ ra biển.
+    Nối liền trung tâm du lịch & trung tâm hành chính của thành phố Đà Nẵng.`,
+  },
+  {
+    rentid: 5,
+    rentImg:
+      "https://images.unsplash.com/photo-1493809842364-78817add7ffb?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    rentPrice: 3.9,
+    rentBedroom: 2,
+    rentBathroom: 2,
+    rentFloor: 1,
+    rentPhone: "+84 1235567",
+    rentSquare: 63,
+    rentAddress: "Hiyori Garden Tower, Đường Võ Văn Kiệt, Phường An Hải Đông",
+    rentAddDis: "Sơn Trà",
+    rentAddCity: "Đà Nẵng",
+    rentTitle: `Chuyển nhà về quê sống nên cần bán căn hộ Hiyori 2PN full nội thất đẹp`,
+    rentDescript: `Bán căn hộ 2PN Hiyori Garden Tower full nội thất đẹp, chỉ cần mang quần áo đến ở.
+
+    Thông tin dự án Hiyori Garden Tower.
+    Hiyori Garden Tower Đà Nẵng tọa lạc trên đường Võ Văn Kiệt, phường An Hải Đông, quận Sơn Trà, TP. Đà Nẵng. Khi đi vào hoạt động, dự án sẽ là một công trình có quy mô, điểm nhấn kiến trúc cảnh quan Đà Nẵng và hoàn chỉnh quy hoạch khu vực.
+    
+    Tên dự án: Hiyori Garden Tower.
+    Chủ đầu tư: Công ty CP Bất động sản Sun Frontier (Nhật Bản) và Công ty TNHH MTV Sun Frontier Đà Nẵng.
+    Đơn vị thiết kế: Viện Quy hoạch Xây dựng Đà Nẵng (UPI Đà Nẵng).
+    Thiết kế nội thất: Công ty TNHH Tư vấn Thiết kế Xây dựng Kiến Vạn Tín.
+    Đơn vị thi công: Công ty CP Dinco.
+    Ngân hàng bảo lãnh: Vietcombank Đà Nẵng.
+    Quy mô: Tòa nhà cao 28 tầng nổi, tầng áp mái và 2 tầng hầm.
+    Tổng diện tích: 2077,6m2.
+    Diện tích xây dựng: 1076m2.
+    Mật độ xây dựng: 51,8%.
+    Tổng diện tích sàn: 33376m2.`,
+  },
+  {
+    rentid: 5,
+    rentImg:
+      "https://images.unsplash.com/photo-1519643381401-22c77e60520e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80",
+    rentPrice: 3,
+    rentBedroom: 2,
+    rentBathroom: 2,
+    rentFloor: 1,
+    rentPhone: "+84 1235567",
+    rentSquare: 63,
+    rentAddress: "Dự án Fusion Suites Da Nang Beach",
+    rentAddDis: "Sơn Trà",
+    rentAddCity: "Đà Nẵng",
+    rentTitle: `Bán căn hộ Fusion Suite , Võ Nguyen Giáp thành phố Đà Nẵng sở hưu lâu dài tiêu chuẩn 4 sao`,
+    rentDescript: `Cần bán căn hộ Fusion suite tâng11,12 view trực diẹn biển giá từ 3.1 tỉ
+    tiện tích : Hồ bơi riêng, bãi tắm riêng, nhà hàng, coffee, spa, yoga riêng của toà căn hộ do Fusion Suite vận hành.`,
+  },
+  {
+    rentid: 5,
+    rentImg:
+      "https://images.unsplash.com/photo-1489171078254-c3365d6e359f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80",
+    rentPrice: 4.32,
+    rentBedroom: 3,
+    rentBathroom: 3,
+    rentFloor: 1,
+    rentPhone: "+84 1235567",
+    rentSquare: 132,
+    rentAddress: "Dự án Blooming Tower, Đường Xuân Diệu, Phường Thuận Phước",
+    rentAddDis: "Hải Châu",
+    rentAddCity: "Đà Nẵng",
+    rentTitle: `Bán chung cư Blooming Tower Đà Nẵng 3 phòng ngủ`,
+    rentDescript: `
+    Bán chung cư Blooming Tower Đà Nẵng. Căn hộ rộng 135m2, bao gồm 1 phòng khách, 1 phòng bếp, 3 phòng ngủ, 2 phòng vệ sinh. View đẹp, tầng cao. Nội thất đẹp đầy đủ, chỉ xách vali vào ở.
+    Chung cư Blooming nằm ngay sát chân cầu Thuận Phước, gần biển Nguyễn Tất Thành. Vị trí đẹp, yên tĩnh, mát mẻ.
+    Giá tốt để đầu tư.
+    Giá bán: 32 triệu/m2 = 4.32 tỷ.
+    Thông tin liên hệ: Công ty TNHH bất động sản Toàn Huy Hoàng.
+    Địa chỉ: Tầng 4 Kim Sơn Building, 16 - 18 Phan Thành Tài - Q. Hải Châu - TP Đà Nẵng.`,
+  },
+  {
+    rentid: 2,
+    rentImg:
+      "https://images.unsplash.com/photo-1510627489930-0c1b0bfb6785?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    rentPrice: 4.6,
+    rentBedroom: 2,
+    rentBathroom: 2,
+    rentFloor: 1,
+    rentPhone: "+84 1235567",
+    rentSquare: 108,
+    rentAddress: "Dự án Lakeview City, Đường Song Hành, Phường An Phú",
+    rentAddDis: "Hải Châu",
+    rentAddCity: "Đà Nẵng",
+    rentTitle: `Bán căn hộ Indochina đường Bạch Đằng, quận Hải Châu, thành phố Đà Nẵng`,
+    rentDescript: `Căn hộ tầng cao rộng 108m2, view đẹp. Thiết kế gồm có 1 phòng khách, 1 phòng bếp, 2 phòng ngủ và 2 phòng vệ sinh. Full nội thất đẹp.
+    Căn hộ Indochina là căn hộ cao cấp nằm ngay trung tâm thành phố, sát sông Hàn. Đầy đủ tiện ích, công năng sử dụng. An ninh 24/24.
+    Thuận tiện giao thương đi lại.
+    Thích hợp mua ở hoặc đầu tư.
+    Giá bán: 4.6 tỷ.
+    Thông tin liên hệ: Công ty TNHH bất động sản Toàn Huy Hoàng.
+    Địa chỉ: Tầng 4 Kim Sơn Building, 16 - 18 Phan Thành Tài - Q. Hải Châu - TP Đà Nẵng.`,
+  },
+  {
+    rentid: 1,
+    rentImg:
+      "https://images.unsplash.com/photo-1542853647-47ad77242390?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    rentPrice: 2.25,
+    rentBedroom: 2,
+    rentBathroom: 2,
+    rentFloor: 1,
+    rentPhone: "+84 1235567",
+    rentSquare: 63,
+    rentAddress: "Dự án F.Home, Phố Lý Thường Kiệt, Phường Thạch Thang",
+    rentAddDis: "Hải Châu",
+    rentAddCity: "Đà Nẵng",
+    rentTitle: `Sang nhượng căn hộ F.Home Lý Thường Kiệt, full nội thất giá sụp hầm covid`,
+    rentDescript: `Bán căn hộ Fhome giá tốt để mua đầu tư hoặc ở, do nhà có việc gấp nên bán nhanh cho khách thiện chí mua thực sựm không đi xem dạo giá cho vui.
+    Khu căn hộ cho khách nước ngoài thuê 80%, tiện ích nội khu có sẵn hồ bơi, phòng gym, spa, trường học quốc tế. Căn hộ ngay trung tâm thành phố rất thuận tiện cho khách hàng di chuyển đưa đón con đi học hoặc đi làm.
+    Diện tích căn hộ: 63m2.
+    Thiết kế: 2 phòng ngủ, 1 phòng khách, 1 phòng bếp, 2 wc.
+    Pháp lý : sổ đỏ chính chủ
+    Hỗ trợ vay ngân hàng từ 60-70% giá trị căn hộ`,
+  },
 ];
